02b2cb9008c556627bae6895c0df02407bcc0a81
Fernando Unger <fdasilvaunger@gmail.com>
2022-09-19 00:11:59 -0300

    feat: output css file and style configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,16 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
-
+      colors: {
+        'facebook-blue': '#1877F2',
+        'facebook-gray': '#F0F2F5',
+        'facebook-green': '#46B72A',
+        'facebook-dark-green': '#36A420',
+      },
+      spacing: {
+        '580': '580px',
+        '380': '380px',
+      },
     },
   },
   plugins: [],
